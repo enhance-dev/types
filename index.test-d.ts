b74a84bde95a8c4091246ac9af814407660a804e
Taylor Beseda <tbeseda@gmail.com>
2024-02-02 11:17:07 -0700

    add preflight fn type

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,104 +1,113 @@
 import { expectError } from "tsd";
 import type {
-	EnhanceApiFn,
-	EnhanceApiFnChain,
-	EnhanceApiRes,
-	EnhanceElemFn,
-	EnhanceHeadFn,
-	EnhanceHtmlFn,
+  EnhanceApiFn,
+  EnhanceApiFnChain,
+  EnhanceApiRes,
+  EnhanceElemFn,
+  EnhanceHeadFn,
+  EnhanceHtmlFn,
+  EnhancePreflightFn,
 } from ".";
 
+// Preflight
+export const preflight: EnhancePreflightFn = function (req) {
+  const { method, path } = req;
+  const title = `Todos — ${path} `;
+  console.log(`Handling ${method} ${path}...`);
+  return { title };
+};
+
 type Todo = {
-	title: string;
-	completed?: boolean;
+  title: string;
+  completed?: boolean;
 };
 
 // API Handler
 // sync
 export const get: EnhanceApiFn = function (request) {
-	console.log(`Handling ${request.path}...`);
+  console.log(`Handling ${request.path}...`);
 
-	const filter: string = request.body.filter as string;
+  const filter: string = request.body.filter as string;
 
-	const todos: Todo[] = [
-		{ title: "todo 1", completed: false },
-		{ title: "todo 2", completed: true },
-		{ title: "todo 3" },
-	];
+  const todos: Todo[] = [
+    { title: "todo 1", completed: false },
+    { title: "todo 2", completed: true },
+    { title: "todo 3" },
+  ];
 
-	let invalidResponse: EnhanceApiRes = { json: { todos } };
-	expectError(
-		(invalidResponse = {
-			json: { foo: 'bar' },
-			body: "",
-		}),
-	);
+  let invalidResponse: EnhanceApiRes = { json: { todos } };
+  expectError(
+    (invalidResponse = {
+      json: { foo: "bar" },
+      body: "",
+    }),
+  );
 
-	return {
-		session: {},
-		status: 200,
-		cacheControl: "public, max-age=14400",
-		json: { filter, todos },
-	};
+  return {
+    session: {},
+    status: 200,
+    cacheControl: "public, max-age=14400",
+    json: { filter, todos },
+  };
 };
 // async
 export const post: EnhanceApiFn = async function (req) {
-	return {
-		json: { req },
-	};
+  return {
+    json: { req },
+  };
 };
 // chain
 export const patch: EnhanceApiFnChain = [
-	(req) => {
-		return { session: { foo: "bar", ...req.session } };
-	},
-	async (req) => {
-		return { json: { path: req.path } };
-	},
-	async () => {
-		return { status: 302, location: "/" };
-	},
+  (req) => {
+    return { session: { foo: "bar", ...req.session } };
+  },
+  async (req) => {
+    return { json: { path: req.path } };
+  },
+  async () => {
+    return { status: 302, location: "/" };
+  },
 ];
 
 // Custom Element
 export const TodoItem: EnhanceElemFn = function ({
-	html,
-	state: { attrs, store, instanceID, context },
+  html,
+  state: { attrs, store, instanceID, context },
 }) {
-	const todoId = attrs["todo-id"];
-	const completed = typeof attrs.completed === "string";
-	const myHtml: EnhanceHtmlFn = html;
-	typeof myHtml === "function";
+  const todoId = attrs["todo-id"];
+  const completed = typeof attrs.completed === "string";
+  const myHtml: EnhanceHtmlFn = html;
+  typeof myHtml === "function";
 
-	context.todoId = todoId;
+  context.todoId = todoId;
 
-	store.myCustomData;
+  store.myCustomData;
 
-	return html`
-		<div class="flex gap-2 mb-1">
-			<input
-				id="todo-${instanceID}"
-				todo-id="${todoId}"
-				type="checkbox"
-				name="completed"
-				${completed ? "checked" : ""}
-			/>
-			<slot></slot>
-		</div>
+  return html`
+    <div class="flex gap-2 mb-1">
+      <input
+        id="todo-${instanceID}"
+        todo-id="${todoId}"
+        type="checkbox"
+        name="completed"
+        ${completed ? "checked" : ""}
+      />
+      <slot></slot>
+    </div>
   `;
 };
 
 // Head Function
 export const Head: EnhanceHeadFn = function (state) {
-	const { req, status, error, store } = state;
-	if (status > 399 && error) {
-		return error;
-	}
+  const { req, status, error, store } = state;
+  if (status > 399 && error) {
+    return error;
+  }
 
-	const { path } = req;
-	const title = `Todos — ${path} - ${store.docTitle}`;
+  const { path } = req;
+  const title = `Todos — ${path} - ${store.docTitle}`;
 
-	return `
+  return `
 <!DOCTYPE html>
 <html>
 <head>
